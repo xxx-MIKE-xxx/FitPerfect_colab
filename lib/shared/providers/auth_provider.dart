2b5120b4e6da53d632ffc53da40eaaf59bf76c77
xxx-MIKE-xxx <89149188+xxx-MIKE-xxx@users.noreply.github.com>
2025-07-20 09:50:33 +0200

    Fix social login handling

--- lib/shared/providers/auth_provider.dart
+++ lib/shared/providers/auth_provider.dart
@@ -120,8 +120,12 @@ class AuthNotifier extends StateNotifier<AuthState> {
 
   Future<void> signInWithProvider(AuthProvider provider) async {
     try {
-      await Amplify.Auth.signInWithWebUI(provider: provider);
-      state = const AuthState.authenticated();
+      final res = await Amplify.Auth.signInWithWebUI(provider: provider);
+      if (res.isSignedIn) {
+        state = const AuthState.authenticated();
+      } else {
+        await _checkLoginStatus();
+      }
     } on AuthException catch (e) {
       state = AuthState.error(e.message);
     }
